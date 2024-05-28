b1ba14ec525e5ef6959f7b28f027daa2abd35c2f
divyaareddyy1998 <divyaareddyy1998@gmail.com>
2024-05-28 15:51:27 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,39 @@
+{
+  /* <div id="parent">
+    <div id="child">
+        <h1>Im h1 tag</h1>
+        <h2>Im h2 tag</h2>
+    </div>
+    <div id="child2">
+        <h1>Im h1 tag</h1>
+        <h2>Im h2 tag</h2>
+    </div>
+</div> */
+}
+
+const main = React.createElement(
+  "div",
+  { id: "parent" },
+  [  React.createElement(
+    "div",
+    { id: "child" },
+    [ React.createElement("h1", {}, "im h1 tag"), React.createElement("h2", {}, "im h2 tag")]
+   
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    [ React.createElement("h1", {}, "im h1 tag"), React.createElement("h2", {}, "im h2 tag")]
+   
+  )]
+
+);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", name: "divya" },
+//   "Welcome to React!!!!"
+// );
+console.log(main); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(main);
