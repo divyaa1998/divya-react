6b42c68cbb48cf81293f273cd462a2713eb9ac0b
divyaareddyy1998 <divyaareddyy1998@gmail.com>
2024-05-30 12:30:10 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,41 +1,34 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
-{
-  /* <div id="parent">
-    <div id="child">
-        <h1>Im h1 tag</h1>
-        <h2>Im h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>Im h1 tag</h1>
-        <h2>Im h2 tag</h2>
-    </div>
-</div> */
-}
+import ReactDOM from "react-dom/client";
 
-const main = React.createElement(
-  "div",
-  { id: "parent" },
-  [  React.createElement(
-    "div",
-    { id: "child" },
-    [ React.createElement("h1", {}, "im divya tag"), React.createElement("h2", {}, "im ndfkjgkdf tag")]
-   
-  ),
-  React.createElement(
-    "div",
-    { id: "child2" },
-    [ React.createElement("h1", {}, "im h1 tag"), React.createElement("h2", {}, "im h2 tag")]
-   
-  )]
+//React.createElement => object => HTMLelement(render)
 
+// const heading = React.createElement('h1',{id:'heading'},"Hello from  React");
+
+// console.log(heading);
+
+//react element
+const Jsxheading = () => (
+  <h1  className="hello" tabIndex={1}>
+    Hello to React from JSX
+  </h1>
 );
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", name: "divya" },
-//   "Welcome to React!!!!"
-// );
-console.log(main); //object
+//react component
+// const HeadingComponent = () => {
+//     return <h1>Heading from React Functional Component</h1>
+// }
+const number = 163551263;
+const HeadingComponent = () => (
+  <div id="container">
+    {console.log('divya')}
+    {Jsxheading()}
+    <Jsxheading />
+    <Jsxheading></Jsxheading>
+    <h1 id="heading">Heading from React Functional Component</h1>
+  </div>
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(main);
+root.render(<HeadingComponent />);
